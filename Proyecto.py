d3914c018e0aee832bd44a7f091b6b4396c8ae9c
Elian Lara <laraelian52@gmail.com>
2025-07-30 22:43:51 -0500

    Archivos txt y NodoArbol

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,25 @@
 print ("Hola Mundo")
+
+import os
+import getpass
+import re
+import hashlib
+import random
+import time
+from datetime import datetime
+from collections import deque
+
+RUTA_NINJAS="ninjas.txt"
+RUTA_HABILIDADES="habilidades_ninja.txt"
+RUTA_USUARIOS="usuarios.txt"
+RUTA_COMBATES="combates.txt"    
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class NodoArbol:
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+        self.izquierda = None
+        self.derecha = None
+
