3ba79081633d054d48678912507734d60710772c
Milka Borja <milk4borj4o@gmail.com>
2025-07-31 19:14:20 -0500

    Agregue clase ninja

--- Proyecto.py
+++ Proyecto.py
@@ -30,3 +30,59 @@ class NodoHabilidad:
         nodo.izquierda = NodoHabilidad.from_string(data_list)
         nodo.derecha = NodoHabilidad.from_string(data_list)
         return nodo
+
+class Ninja:
+    
+    def __init__(self, nombre, fuerza, agilidad, resistencia, estilo, puntos_victoria=0):
+        self.nombre = nombre
+        self.fuerza = int(fuerza)
+        self.agilidad = int(agilidad)
+        self.resistencia = int(resistencia)
+        self.estilo = estilo
+        self.puntos_victoria = int(puntos_victoria)
+        self.arbol_habilidades = None  # Aquí se almacena el árbol de habilidades
+
+    def __str__(self):
+        return (f"Nombre: {self.nombre}, Fuerza: {self.fuerza}, Agilidad: {self.agilidad}, "
+                f"Resistencia: {self.resistencia}, Estilo: {self.estilo}, Puntos de Victoria: {self.puntos_victoria}")
+
+    # Método para guardar solo nombre y estilo en ninjas.txt
+    def to_file_string_basic(self):
+        return f"{self.nombre},{self.fuerza},{self.agilidad},{self.resistencia},{self.estilo},{self.puntos_victoria}"
+
+    # Método para guardar las habilidades en habilidades_ninja.txt
+    def to_habilidades_string(self):
+        if self.arbol_habilidades:
+            habilidades_list = []
+            def serialize_tree_preorder(node):
+                if node:
+                    habilidades_list.append(f"{node.nombre},{node.puntos}")
+                    serialize_tree_preorder(node.izquierda)
+                    serialize_tree_preorder(node.derecha)
+                else:
+                    habilidades_list.append("None")
+            serialize_tree_preorder(self.arbol_habilidades)
+            return f"{self.nombre}{HABILIDAD_DELIMITER}{HABILIDAD_DELIMITER.join(habilidades_list)}"
+        return f"{self.nombre}{HABILIDAD_DELIMITER}None" # Si no tiene habilidades, igual se guarda el nombre
+
+    @staticmethod
+    def from_file_string_basic(line):
+        
+        data = line.strip().split(',')
+        if len(data) == 6: # Ahora son 6 campos (nombre, fuerza, agilidad, resistencia, estilo, puntos_victoria)
+            return Ninja(data[0], data[1], data[2], data[3], data[4], data[5])
+        return None
+
+    @staticmethod
+    def from_habilidades_string(line):
+        
+        parts = line.strip().split(HABILIDAD_DELIMITER, 1) # Separamos solo el primer delimitador
+        if len(parts) == 2:
+            nombre_ninja = parts[0]
+            habilidades_str = parts[1]
+            if habilidades_str != "None":
+                habilidades_list = habilidades_str.split(HABILIDAD_DELIMITER)
+                arbol_habilidades = NodoHabilidad.from_string(habilidades_list)
+                return nombre_ninja, arbol_habilidades
+        return None, None
+    
